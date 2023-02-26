10f9effe28cc1de655c102f971afb02d9441af50
lmnhut10 <nhutlmgcc200295@fpt.edu.vn>
2023-02-26 15:29:29 +0700

    update lish controller

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -14,16 +14,15 @@ class ListController extends AbstractController
     public function index(Request $request)
     {
         $companies = [
-            'Apple' => '$1.16 trillion USD',
-            'Samsung' => '$298.68 billion USD',
-            'Microsoft' => '$1.10 trillion USD',
-            'Alphabet' => '$878.48 billion USD',
+            'Marshall Killburn' => '$350 trillion USD',
+            'Marshall Stanmore 2' => '$500 billion USD',
+            'Marshall Stockwell 2' => '$250 trillion USD',
+            'Marshall Tuffon' => '$400 billion USD',
             'Intel Corporation' => '$245.82 billion USD',
-            'IBM' => '$120.03 billion USD',
-            'Facebook' => '$552.39 billion USD',
+            'Marshall Woburn 2' => '$600 billion USD',
+            'Marshall Minor 2' => '$200 billion USD',
             'Hon Hai Precision' => '$38.72 billion USD',
-            'Tencent' => '$3.02 trillion USD',
-            'Oracle' => '$180.54 billion USD',
+            'Marshall Major 4' => '$250 trillion USD',
         ];
 
         return $this->render('list/index.html.twig', [
