39c3bd4b70718efdf81963923694825538324b5f
Haile797 <Hailvgcc200225@fpt.edu.vn>
2023-02-28 21:47:13 +0700

    add product

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -37,29 +37,30 @@ class ProductType extends AbstractType
         ;
     }
 }
-    // public function buildForm(FormBuilderInterface $builder, array $options)
-    // {
-    //     $builder
-    //     ->add('name')
-    //     ->add('price')
-    //     ->add('created',DateType::class,[
-    //         'widget' => 'single_text',
-    //         'required'=>false
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('name')
+        ->add('price')
+        ->add('created',DateType::class,[
+            'widget' => 'single_text',
+            'required'=>false
 
-    //     ])
-    //     ->add('quantity')
-    //     ->add('file', FileType::class, [
-    //         'label' => 'Product Image',
-    //         'required'=>false,
-    //         'mapped' => false
-    //     ])
-    //     ->add('image', HiddenType::class, [
-    //         'required'=>false
-    //     ])
-    //     ->add('save', SubmitType::class, [
-    //         'label' => "Confirm"
-    //     ])
-    //     ;
-    // }
+        ]);
+    }
+        ->add('quantity')
+        ->add('file', FileType::class, [
+            'label' => 'Product Image',
+            'required'=>false,
+            'mapped' => false
+        ])
+        ->add('image', HiddenType::class, [
+            'required'=>false
+        ])
+        ->add('save', SubmitType::class, [
+            'label' => "Confirm"
+        ])
+        ;
+    }
 
 ?>
