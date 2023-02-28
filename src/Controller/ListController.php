26082177410882799b306a64271a30a238ac1ec8
datnhism <98210435+datnhism@users.noreply.github.com>
2023-02-28 19:31:55 +0700

    update ListController

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -14,15 +14,15 @@ class ListController extends AbstractController
     public function index(Request $request)
     {
         $companies = [
-            'Marshall Killburn' => '$350 trillion USD',
-            'Marshall Stanmore 2' => '$500 billion USD',
-            'Marshall Stockwell 2' => '$250 trillion USD',
-            'Marshall Tuffon' => '$400 billion USD',
-            'Intel Corporation' => '$245.82 billion USD',
-            'Marshall Woburn 2' => '$600 billion USD',
-            'Marshall Minor 2' => '$200 billion USD',
-            'Hon Hai Precision' => '$38.72 billion USD',
-            'Marshall Major 4' => '$250 trillion USD',
+            'Marshall Killburn' => '$1 million USD',
+            'Marshall Stanmore 2' => '$5 million USD',
+            'Marshall Stockwell 2' => '$2 million USD',
+            'Marshall Tuffon' => '$4 million USD',
+            'Intel Corporation' => '$2 million USD',
+            'Marshall Woburn 2' => '$6 million USD',
+            'Marshall Minor 2' => '$2 million USD',
+            'Hon Hai Precision' => '$3.72 million USD',
+            'Marshall Major 4' => '$2.07 million USD',
         ];
 
         return $this->render('list/index.html.twig', [
